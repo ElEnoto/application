f3bdee069b07c59a6cd99e83ffcda6d3d4549fed
DUbovskiy Max <chedrmax@mail.ru>
2021-12-15 21:10:49 +0300

    SQL Index

---
+++ OTUS.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE otus;db
+USE otus;
+CREATE TABLE authors_books ( id_author_book int, id_author INT, id_book INT );
+
+CREATE TABLE books ( id_book int, name_book VARCHAR (50), page_number INT, year_of_publishing DATE );
