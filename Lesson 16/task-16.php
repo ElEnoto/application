68cb0fe23ec573ea1801ea668fdffdbf3923ce38
DUbovskiy Max <chedrmax@mail.ru>
2022-01-16 16:14:57 +0300

    studied Fiels; completed the hw for lesson 17

--- Lesson 16/task-16.php
+++ Lesson 16/task-16.php
@@ -1,37 +1,21 @@
 <?php
 
-$dir1 = "jpg";
-$a = opendir($dir1);
-$b = scandir($dir1);
-foreach ($b as $value){
+$dir = "jpg";
+$open = opendir($dir);
+$scan = scandir($dir);
+foreach ($scan as $value){
     if ($value == "." or $value == "..") {
-        unset($b[array_search($value,$b)]);
+        unset($scan[array_search($value,$scan)]);
     }
 }
 
-$dir2 = "html";
-$c = opendir($dir2);
-$d = scandir($dir2);
-foreach ($d as $value){
-    if ($value == "." or $value == "..") {
-        unset($d[array_search($value,$d)]);
-    }
+foreach ($scan as $jpg) {
+    $name = "jpg/" . "$jpg";
+
+    echo "<a href = $name target='_blank'><img src=$name style='width:280px; height: 200px; padding-right: 20px'></a>";
 }
 
-foreach ($b as $jpg) {
-    $f = "jpg/" . "$jpg";
-    $data1[] = $f;
-}
 
-foreach ($d as $html) {
-    $h = "html/" . "$html";
-    $data2[] = $h;
-}
-
-for ($i=0, $j=0; $i <= (count($data1)-1), $j <= (count($data2)-1); $i++, $j++){
-
-echo "<a href = $data2[$j] target='_blank'><img src=$data1[$i] style='width:280px; height: 200px; padding-right: 20px'></a>";
-}
 ?>
 
 
