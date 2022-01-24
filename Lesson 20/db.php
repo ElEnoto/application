cef2d4143aa544fc86f16bd5ff465259f62f4f5e
DUbovskiy Max <chedrmax@mail.ru>
2022-01-25 01:27:04 +0300

    library + photos

---
+++ Lesson 20/db.php
@@ -0,0 +1,158 @@
+<?php
+declare(strict_types=1);
+
+
+function db_connect() {
+    return new PDO('pgsql:host=localhost;dbname=otus','postgres','otus',[
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+}
+
+
+function authenticate($username, $password) {
+    $pdo = db_connect();
+    $result = $pdo->prepare('select id from users where username = ? and password = ?');
+    $result->execute([$username, md5($password)]);
+    if($result->rowCount() == 0)
+        return false;
+    return $result->fetchAll()[0]['id'];
+}
+
+function set_remember_token($user_id, $token) {
+    $pdo = db_connect();
+    $result = $pdo->prepare('update users set remember_token = ? where id = ?');
+    $result->execute([$token,$user_id]);
+}
+
+function authenticate_by_token($token) {
+    $pdo = db_connect();
+    $result = $pdo->prepare('select id, username from users where remember_token = ?');
+    $result->execute([$token]);
+    if($result->rowCount() == 0)
+        return false;
+    return $result->fetchAll()[0];
+}
+
+
+function add_photo($user_id, $files, $copies)
+{
+    $pdo = db_connect();
+    $result = $pdo->prepare('insert into photo(user_id, files, copies, date) values (?,?,?,?)');
+    $result->execute([$user_id, $files, $copies, date('Y-m-d')]);
+}
+
+
+function getAllInformation() {
+    $pdo = db_connect();
+    $result = $pdo->query('select books.id_book, picture_copy, picture_file, name_book, first_name, last_name, page_number, 
+       year_of_publishing from books
+	   JOIN authors_books ON books.id_book=authors_books.id_book 
+        JOIN authors ON authors_books.id_author=authors.id_author
+        Join picture ON picture.id_book = books.id_book
+	   order by books.id_book
+');;
+    $result->execute();
+    $data = $result->fetchAll();
+    return $data;
+}
+
+function getSomeInformation($request) {
+    $pdo = db_connect();
+    $result = $pdo->query("select * from (select books.id_book, picture_copy, picture_file, name_book, first_name, last_name, page_number, 
+                      year_of_publishing from books
+	   JOIN authors_books ON books.id_book=authors_books.id_book JOIN authors ON authors_books.id_author=authors.id_author
+        Join picture ON picture.id_book = books.id_book
+	   order by books.id_book) as foo 
+where (lower (name_book) like lower (('%$request%')) or lower (first_name)  like lower (('%$request%')) or lower (last_name)  like lower (('%$request%')) )");
+    $result->execute();
+    $data = $result->fetchAll();
+    return $data;
+}
+
+function insertContentAuthors(array $content) {
+    $pdo = db_connect();
+    $lastname = $content['LASTNAME'];
+    $firstname = $content['FIRSTNAME'];
+
+
+    $result = $pdo->prepare('insert into authors (last_name, first_name) values (?, ?);');
+    $result->execute([$lastname, $firstname]);
+
+}
+
+function insertContentBooks(array $content) {
+    $pdo = db_connect();
+
+    $name_book = $content['NAME_BOOK'];
+    $page_number = (int)$content['PAGE_NUMBER'];
+    $year_of_publishing = $content['YEAR_OF_PUBLISHING'];
+
+
+    $result = $pdo->prepare(' 
+insert into books (name_book, page_number, year_of_publishing) values (?, ?, ?); 
+');
+    $result->execute([$name_book, $page_number, $year_of_publishing]);
+
+}
+
+function insertContentAuthorsBooks(array $content) {
+    $pdo = db_connect();
+    $lastname = $content['LASTNAME'];
+    $firstname = $content['FIRSTNAME'];
+    $name_book = $content['NAME_BOOK'];
+
+
+    $result = $pdo->prepare(" 
+insert into authors_books (id_author, id_book) values ((select id_author from authors where last_name like '$lastname' and first_name like '$firstname'), 
+													   (select id_book from books where name_book like  '$name_book'));
+");
+    $result->execute();
+
+}
+
+function insertContentPicture(array $content) {
+    $pdo = db_connect();
+
+    $name_book = $content['NAME_BOOK'];
+
+    $picture_file = $content['PICTURE_FILE'];
+    $picture_copy = $content['PICTURE_COPY'];
+
+    $result = $pdo->prepare('
+insert into picture (id_book, picture_file, picture_copy) values ((select id_book from books where name_book = ?), 
+																?, ?)');
+    $result->execute([$name_book, $picture_file, $picture_copy]);
+
+}
+
+function deleteAll (){
+    $pdo = db_connect();
+    $result = $pdo->query('delete from bookshelves_bookcases');
+    $result->execute();
+}
+
+function deletePicture ($contentId)
+{
+    $pdo = db_connect();
+    $result = $pdo->query('delete from picture where id_book = ?');
+    $result->execute($contentId);
+}
+
+function deleteAuthors ($contentId)
+{
+    $pdo = db_connect();
+    $result = $pdo->query('delete from authors where id_author = (select id_author from authors_books where id_book = ?)');
+    $result->execute($contentId);
+}
+
+
+function deleteBooks ($contentId)
+{
+    $pdo = db_connect();
+    $result = $pdo->query('delete from books where id_book = ?');
+    $result->execute($contentId);
+}
+
+
+
---
+++ Lesson 20/index.php
@@ -0,0 +1,86 @@
+<?php
+declare(strict_types=1);
+session_start();
+require_once 'db.php';
+require_once "function.php";
+?>
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="../bootstrap/css/bootstrap-grid.css">
+<link rel="stylesheet" href="../bootstrap/css/bootstrap-reboot.css">
+<link rel="stylesheet" href="style.css">
+<?php
+if (!empty($_COOKIE['remember_token']) && empty($_SESSION['user_id'])) {
+    $result = authenticate_by_token($_COOKIE['remember_token']);
+    if ($result) {
+        $_SESSION['user_id'] = $result['id'];
+        $_SESSION['name'] = $result['username'];
+    }
+}
+if (!empty($_GET['action']) && $_GET['action'] == 'auth' && empty($_SESSION['user_id'])) {
+    $result = authenticate($_POST['username'], $_POST['password']);
+    if (!$result) {
+        echo '<h2>Невреное имя пользователя или пароль!</h2><br>';
+    } else {
+        $_SESSION['user_id'] = $result;
+        $_SESSION['name'] = $_POST['username'];
+        if (!empty($_POST['remember_me']) && $_POST['remember_me'] == 'on') {
+            $token = uniqid();
+            set_remember_token($_SESSION['user_id'], $token);
+            setcookie('remember_token', $token, time() + 60 * 60 * 24 * 30);
+        }
+    }
+}
+
+if (empty($_SESSION['user_id'])) { ?>
+    <div style="padding: 15px">
+        <h2>Для просмотра библиотеки вы должны войти в систему!</h2><br>
+        <form action="index.php?action=auth" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="username">Имя пользователя:</label>
+                <input type="text" class="form-control" name="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Пароль:</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" name="remember_me">
+                <label for="remember_me">Запомнить меня:</label>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Войти в систему">
+        </form>
+    </div>
+    <?php
+} else {
+    echo "<h2 style='padding: 15px'>Добро пожаловать {$_SESSION['name']}</h2>";
+    if ($_SESSION['name'] == 'admin') { ?>
+        <div style="padding: 15px">
+            <form action="index.php?action=download_file" method="post" enctype="multipart/form-data">
+                <input type="text" name="firstname" placeholder="Имя" /><br>
+                <input type="text" name="lastname" placeholder="Фамилия" /><br>
+                <input type="text" name="name_book" placeholder="Название книги" /><br>
+                <input type="text" name="page_number" placeholder="Количество страниц" /><br>
+                <input type="text" name="year_of_publishing" placeholder="Год издания" /><br>
+
+                <div class="form-group">
+                    <input type="file" class="form-control-file" name="photo">
+                    <input type="submit" value="Загрузить данные">
+                </div>
+                <div>
+                    <input class="form-control" type="text" name="title" placeholder="Введите название книги или ФИО автора"><br>
+                    <input type="submit" class="btn btn-primary" value="Поиск">
+                </div>
+            </form>
+        </div>
+        <?php addBook();
+
+    } else {?>
+        <form action="index.php" method="post">
+            <input class="form-control" type="text" name="title" placeholder="Введите название книги или ФИО автора"><br>
+            <input type="submit" class="btn btn-primary" value="Поиск">
+        </form>
+        <?php showAllBooksForUsers();
+    }
+}
+?>
+
