593e72636cb2fe18b28d2d002a80b582f044c89c
DUbovskiy Max <chedrmax@mail.ru>
2022-01-30 13:29:46 +0300

    Class

--- Lesson 22/index.php
+++ Lesson 22/index.php
@@ -1,12 +1,13 @@
 <?php
 class Product
 {
-    public $mfr, $barcode, $type, $name, $price, $color, $weight, $height, $width, $thickness;
-    public function __construct($mfr, $barcode, $type, $name, $price, $color, $weight, $height, $width, $thickness)
+    public $mfr, $barcode, $type, $class, $name, $price, $color, $weight, $height, $width, $thickness;
+    public function __construct($mfr, $barcode, $type, $class, $name, $price, $color, $weight, $height, $width, $thickness)
     {
         $this->mfr = $mfr;
         $this->barcode = $barcode;
         $this->type = $type;
+        $this->class = $class;
         $this->name = $name;
         $this->price = $price;
         $this->color = $color;
@@ -21,6 +22,7 @@ class Product
         echo "Страна изготовитель: $this->mfr;" ."<br>";
         echo "Штрих-код: $this->barcode;" ."<br>";
         echo "Тип товара: $this->type;" ."<br>";
+        echo "Класс товара: $this->class;" ."<br>";
         echo "Наименование товара: $this->name;" ."<br>";
         echo "Стоимость: $this->price рублей;" ."<br>";
         echo "Цвет товара: $this->color;" ."<br>";
@@ -32,15 +34,65 @@ class Product
 
 
 class WashingMachine extends Product
+// Мы знаем, что все стиральные машины - это бытовая техника, поэтому type и class указывать не нужно,
+// онb одинаковый
 {
+    public function __construct($mfr, $barcode, $name, $price, $color, $weight, $height, $width, $thickness){
+        $this->mfr = $mfr;
+        $this->barcode = $barcode;
+        $this->name = $name;
+        $this->price = $price;
+        $this->color = $color;
+        $this->weight = $weight;
+        $this->height = $height;
+        $this->width = $width;
+        $this->thickness = $thickness;
+    }
+    public function info()
+    {
+        echo "Страна изготовитель: $this->mfr;" ."<br>";
+        echo "Штрих-код: $this->barcode;" ."<br>";
+        echo "Тип товара: Бытовая техника;" ."<br>";
+        echo "Класс товара: Стиральная машина;" ."<br>";
+        echo "Наименование товара: $this->name;" ."<br>";
+        echo "Стоимость: $this->price рублей;" ."<br>";
+        echo "Цвет товара: $this->color;" ."<br>";
+        echo "Вес: $this->weight кг;" ."<br>";
+        echo "Габариты: $this->height мм х $this->width мм х $this->thickness мм";
+    }
+}
+class Indesit extends WashingMachine
+// Мы знаем, что стиральные машины Indesit производятся в Италии, поэтому $mfr указывать не нужно,
+// он одинаковый
+{
+    public function __construct($barcode, $name, $price, $color, $weight, $height, $width, $thickness){
+        $this->barcode = $barcode;
+        $this->name = $name;
+        $this->price = $price;
+        $this->color = $color;
+        $this->weight = $weight;
+        $this->height = $height;
+        $this->width = $width;
+        $this->thickness = $thickness;
+    }
+    public function info()
+    {
+        echo "Страна изготовитель: Италия;" ."<br>";
+        echo "Штрих-код: $this->barcode;" ."<br>";
+        echo "Тип товара: Бытовая техника;" ."<br>";
+        echo "Класс товара: Стиральная машина;" ."<br>";
+        echo "Наименование товара: $this->name;" ."<br>";
+        echo "Стоимость: $this->price рублей;" ."<br>";
+        echo "Цвет товара: $this->color;" ."<br>";
+        echo "Вес: $this->weight кг;" ."<br>";
+        echo "Габариты: $this->height мм х $this->width мм х $this->thickness мм";
+    }
 
 }
-$a1 = new WashingMachine ('Италия', '854632197000256', 'Бытовая техника',
-    'Indesit', '15499', 'Белый', '62', '850', '600', '400');
-$a1 -> info();
 
-
-
+$a2 = new Indesit ('854632197000256','Indesit iwsd 51051', '15499',
+    'Белый', '62', '850', '600', '400');
+$a2 -> info();
 
 
 
