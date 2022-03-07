2dd5abfaaa6cc3ace91c98fb0f252288e9057ab9
DUbovskiy Max <chedrmax@mail.ru>
2022-03-07 19:25:08 +0300

    SOLID

--- Lesson_26/www/src/App.php
+++ Lesson_26/www/src/App.php
@@ -4,7 +4,7 @@ namespace Otus;
 use Otus\Controllers\RouteController;
 
 class App {
-    public static function run():mixed
+    public static function run():void
     {
         $controller = new RouteController();
         $route = $controller->route();
@@ -13,9 +13,11 @@ class App {
         if(!class_exists($controller_name,true) or !method_exists($controller_name, $action_name)) {
             View::$name = 'Something wrong';
             View::$title = '404 - Not Foud';
-            return View::template();
+            View::template();
+        } else {
+            $controller = new $controller_name();
+            $controller->$action_name();
         }
-        $controller = new $controller_name();
-        return $controller->$action_name();
+
     }
 }
