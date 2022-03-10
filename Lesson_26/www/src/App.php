d1626ba8d8cdc37de298868b45a1091f571bed81
DUbovskiy Max <chedrmax@mail.ru>
2022-03-10 19:28:27 +0300

    SOLID

--- Lesson_26/www/src/App.php
+++ Lesson_26/www/src/App.php
@@ -4,12 +4,11 @@ namespace Otus;
 use Otus\Controllers\RouteController;
 
 class App {
-    public static function run():void
+    public static function run()
     {
-        $controller = new RouteController();
-        $route = $controller->route();
-        $controller_name = $route[0];
-        $action_name = $route[1];
+        RouteController::route();
+        $controller_name = RouteController::$controller_name;
+        $action_name = RouteController::$action_name;
         if(!class_exists($controller_name,true) or !method_exists($controller_name, $action_name)) {
             View::$name = 'Something wrong';
             View::$title = '404 - Not Foud';
@@ -18,6 +17,5 @@ class App {
             $controller = new $controller_name();
             $controller->$action_name();
         }
-
     }
 }
--- Lesson_26/www/src/Models/Task_tracker/Find.php
+++ Lesson_26/www/src/Models/Task_tracker/Find.php
@@ -12,15 +12,19 @@ class Find
             return $add_content;
         }
         if (!empty($_POST['id']) and !empty($_POST['task']) and !empty($_POST['firstname']) and !empty($_POST['lastname'])) {
-            if (preg_match('/^\+?\d+$/', $_POST['id'])) {
+            if (!preg_match('/^\+?\d+$/', $_POST['id'])) {
+                Find::$error = 'Поле "Номер" должно быть числом';
+                return Find::$error;
+            } elseif (!preg_match('/^[a-zа-я_-]$/', $_POST['firstname']) or !preg_match('/^[a-zа-я_-]$/', $_POST['lastname'])){
+                Find::$error = 'Поле "firstname" и "lastname" должны состоять из букв';
+                return Find::$error;
+            }
+            else {
                 $add_content['ID'] = $_POST['id'];
                 $add_content['task'] = $_POST['task'];
                 $add_content['last_name'] = $_POST['lastname'];
                 $add_content['first_name'] = $_POST['firstname'];
                 return $add_content;
-            } else {
-                Find::$error = 'Поле "Номер" должно быть числом';
-                return Find::$error;
             }
         }
         else {
